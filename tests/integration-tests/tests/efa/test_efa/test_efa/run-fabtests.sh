f5a93b221462ca55b987050f6ab102a0d0f2e5c9
hanwenli <hanwenli@amazon.com>
2025-09-15 13:07:40 -0700

    fabtest temporary change

--- tests/integration-tests/tests/efa/test_efa/test_efa/run-fabtests.sh
+++ tests/integration-tests/tests/efa/test_efa/test_efa/run-fabtests.sh
@@ -47,6 +47,7 @@ python3 $FABTESTS_RUNNER \
   --expression "$TEST_EXPRESSION" \
   --timeout $FABTESTS_TIMEOUT \
   --junit-xml $REPORT_FILE \
+  -E FI_LOG_LEVEL=warn \
   --do-dmabuf-reg-for-hmem \
   -v $TEST_ENVIRONMENT_OPTION $COMPUTE_IP_1 $COMPUTE_IP_2 > $LOG_FILE 2>&1 &
 
